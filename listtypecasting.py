6c2b55acfbcefb86f5958450d1000ebf62cd9453
palak porwal <99019333+39palakved@users.noreply.github.com>
2022-05-11 11:07:20 +0530

    Add files via upload

---
+++ listtypecasting.py
@@ -0,0 +1,5 @@
+#x = "a+b-c/z"
+x = "22+18-19/110"
+print(x)
+y = list(x)
+print(y)
---
+++ module.py
@@ -0,0 +1,20 @@
+import math
+x=int(input("enter value"))
+y=math.sqrt(x)
+print(x)
+print(y)
+import math as m
+x=int(input("enter value"))
+y=m.factorial(x)
+print(x)
+print(y)
+from math import sqrt
+x=int(input("enter value"))
+y = sqrt(x)
+print(x,y)
+from math import *
+x=int(input("enter value"))
+y=math.factorial(x)
+print(x)
+print(y)
+
